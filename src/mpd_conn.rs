a71c1bbc03380fa988266b0a23621ed83ed96ee3
Christian Wicke <github.christian@safersignup.com>
2019-07-19 17:04:27 +0200

    added eprintln for errors

--- src/mpd_conn.rs
+++ src/mpd_conn.rs
@@ -13,7 +13,7 @@ pub struct MpdConnection {
 
 impl MpdConnection {
     pub fn new(addr: &str) -> io::Result<MpdConnection> {
-        let stream = TcpStream::connect(addr)?;
+        let stream = TcpStream::connect(addr).map_err(|e| {eprintln!("Connect to {} failed: {}", addr, e); e})?;
         let reader = io::BufReader::new(stream.try_clone()?);
 
         let mut mpd_c = MpdConnection {
@@ -27,7 +27,7 @@ impl MpdConnection {
 
     fn read_line(&mut self) -> io::Result<String> {
         let mut line = String::new();
-        self.reader.read_line(&mut line)?;
+        self.reader.read_line(&mut line).map_err(|e| {eprintln!("Reading from mpd failed: {}", e); e})?;
         Ok(line)
     }
 
@@ -49,8 +49,8 @@ impl MpdConnection {
 
 impl MpdConn for MpdConnection {
     fn call(&mut self, request: &str) -> io::Result<String> {
-        self.stream.write(request.as_bytes())?;
-        self.read_response()
+        self.stream.write(request.as_bytes()).map_err(|e| {eprintln!("Writing to mpd: {}", e); e})?;
+        self.read_response().map_err(|e| {eprintln!("MPD answered the request \"{}\" with the following error: {}", request, e); e})
     }
 
 }
